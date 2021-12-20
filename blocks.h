0a29a1aa538759aca70e02502c23d3817176cd8b
Vasco Nunes <contact@vasco.dev>
2021-12-20 01:04:06 +0000

    Add weather and crypto blocks

--- blocks.h
+++ blocks.h
@@ -1,16 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	// {"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
-
 	{"", "~/bin/dwmblocks/mpd.sh",						3,		0},
-
 	{"", "~/bin/dwmblocks/mem.sh",	30,		0},
-
-	{"", "~/bin/dwmblocks/sensors.sh",				5,		0},
-
+	// {"", "~/bin/dwmblocks/sensors.sh",				5,		0},
+	{"", "~/bin/dwmblocks/battery.sh",				60,		0},
+	{"", "~/bin/dwmblocks/crypto.sh",				300,		0},
+	{"", "~/bin/dwmblocks/weather.sh",				600,		0},
 	{"", "~/bin/dwmblocks/clock.sh",						1,		0},
-
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
